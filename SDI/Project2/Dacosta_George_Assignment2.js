e099a5a769aac1851a5653237bd01f71ae165dac
georgedacosta <georgedacosta@cfl.rr.com>
2012-11-29 19:24:12 -0500

    Vars added

---
+++ SDI/Project2/Dacosta_George_Assignment2.js
@@ -0,0 +1,37 @@
+/*
+George Dacosta
+SDI 1212
+Project 2
+November 29, 2012
+*/
+
+//Variables
+
+var ourTrip = "Our trip to New York";
+var ourAttractions = [
+					"Museum of Modern Art",
+					"Central Park",
+					"Top of the Rock",
+					"Ground Zero",
+					"Statue of Liberty",
+					"Empire State Building",
+					"Times Square"
+];
+var transport = [
+				"Taxi",
+				"Subway",
+				"City Bus",
+				"Tourist Bus",
+				"walk"	
+];
+var budgetPerTickets = 200;
+var costPerTicket = 75;
+var yes = true;
+var no = false;
+var milesWalked = 5;
+
+
+
+//Procedure
+
+var = function () 
