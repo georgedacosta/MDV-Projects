6d4af6166adb1c8f9edda8944360d4693f985f86
georgedacosta <georgedacosta@cfl.rr.com>
2013-01-20 12:29:21 -0500

    replaced all $ with ge

--- VFW/Project3/Js/main.js
+++ VFW/Project3/Js/main.js
@@ -1,9 +1,9 @@
 /*
 George Dacosta
 VFW 1301
-Project 2
+Project 3
 main.js
-January 17, 2013
+January 24, 2013
 */
 
 
@@ -12,7 +12,7 @@ window.addEventListener("DOMContentLoaded", function(){
 
 
 	//getElementById Function.
-	function $(x){
+	function ge(x){
 		var myElems = document.getElementById(x);
 		return myElems;
 	};
@@ -24,7 +24,7 @@ window.addEventListener("DOMContentLoaded", function(){
 	
 	function makeAppt(){
 		var tarTag = document.getElementsByTagName("form"),   //tarTag is an array of all the form tags.
-			selectListItem = $('dDown'),
+			selectListItem = ge('dDown'),
 			makeDD = document.createElement('select');
 			makeDD.setAttribute("id", "appts");
 		for(var i = 0, j=apptType.length; i<j; i++){
@@ -51,17 +51,17 @@ window.addEventListener("DOMContentLoaded", function(){
 	function togglecontrols(n){
 		switch(n){
 	   	    case "on":
-	   	         $('apptForm').style.display = "none";
-	   	         $('clearDataLink').style.display = "inline";
-	   	         $('displayDataLink').style.display = "none";
-	   	         $('newForm').style.display = "inline";
+	   	         ge('apptForm').style.display = "none";
+	   	         ge('clearDataLink').style.display = "inline";
+	   	         ge('displayDataLink').style.display = "none";
+	   	         ge('newForm').style.display = "inline";
 	   	    	break;
 	   	    case "off":
-	   	         $('apptForm').style.display = "block";
-	   	         $('clearDataLink').style.display = "inline";
-	   	         $('displayDataLink').style.display = "inline";
-	   	         $('newForm').style.display = "none";
-	   	         $('items').style.display = "none";	   	    
+	   	         ge('apptForm').style.display = "block";
+	   	         ge('clearDataLink').style.display = "inline";
+	   	         ge('displayDataLink').style.display = "inline";
+	   	         ge('newForm').style.display = "none";
+	   	         ge('items').style.display = "none";	   	    
 	   	    	break;
 	   	    default:	
 		    	return false;
@@ -83,15 +83,15 @@ window.addEventListener("DOMContentLoaded", function(){
 		getSelectedRadio();
 		var product			={};
 		
-			product.apptName	=["Appointment Name", $('apptName').value];
-			product.apptDate	=["Appointment Date", $('apptDate').value];
-			product.email   	=["Email", $('email').value];
-			product.phone	    =["Phone Number", $('phone').value];
-			product.apptType    =["Appointment Type", $('appts').value];
-  			product.itemName    =["Item Name", $('itemName').value];  
+			product.apptName	=["Appointment Name", ge('apptName').value];
+			product.apptDate	=["Appointment Date", ge('apptDate').value];
+			product.email   	=["Email", ge('email').value];
+			product.phone	    =["Phone Number", ge('phone').value];
+			product.apptType    =["Appointment Type", ge('appts').value];
+  			product.itemName    =["Item Name", ge('itemName').value];  
 			product.itemChange  =["Did Item Change Hands", itemChangeValue];
-			product.amount      =["Item Value", $('amount').value];
-			product.notes       =["Notes:", $('notes').value];
+			product.amount      =["Item Value", ge('amount').value];
+			product.notes       =["Notes:", ge('notes').value];
 		// Save data into Local Storage: Use Stringify to convert object to a string.
 		localStorage.setItem(numbId, JSON.stringify(product));
 		alert("Info Stored!");
@@ -113,6 +113,7 @@ window.addEventListener("DOMContentLoaded", function(){
 		for(var i=0, len=localStorage.length; i<len;i++){
 			var createLi = document.createElement('li');
 			createList.appendChild(createLi);
+			var linkItem = document.createElement('li');
 			var key = localStorage.key(i);
 			var value = localStorage.getItem(key);
 			//Convert the string from Local Storage value back to an object using JSON.parse()
@@ -123,13 +124,41 @@ window.addEventListener("DOMContentLoaded", function(){
 			    var createSubli = document.createElement('li');
 			    createSubList.appendChild(createSubli);
 			    var subText = myObject[n][0]+" "+myObject[n][1];
-			    createSubli.innerHTML = subText;	
+			    createSubli.innerHTML = subText;
+			    createSubli.appendChild(linkItem)
 				
 			}
-			
+			createItemLinks(localStorage.key(i),linkItem); //Create the edit and delete buttons/Link for each item in Local Storage.
 			
 		}
 		
+		
+	}
+	// Create Item Links
+	// Create the edit and delete links for each stored item when displayed.
+	
+	function createItemLinks(key,linkItem){
+		// add edit single item link
+		var linkToEdit = document.createElement('a');
+	    linkToEdit.href = "#";
+		linkToEdit.key = key;
+		var eText = "Edit Info";
+/* 		linkToEdit.addEventListener("click", editAppt); */
+		linkToEdit.innerHTML = eText;
+		linkItem.appendChild(linkToEdit);
+		
+		
+		// add line break
+		var tagBr =document.createElement('br');
+		linkItem.appendChild(tagBr);
+		
+		var deleteItem = document.createElement('a');
+		deleteItem.href = "#";
+		deleteItem.key = key;
+		var delText = "Delete Appointment";
+/* 		deleteItem.addEventListener("click", deleteAppt); */
+		deleteItem.innerHTML = delText;
+		linkItem.appendChild(deleteItem);
 		
 	}
 	
@@ -148,13 +177,13 @@ window.addEventListener("DOMContentLoaded", function(){
 	makeAppt();
 	
 	//Set Link & Submit Click Events
-	var displayDataLink = $('displayDataLink');
+	var displayDataLink = ge('displayDataLink');
 	displayDataLink.addEventListener("click", pullData);
- 	var clearDataLink = $('clearDataLink'); 
+ 	var clearDataLink = ge('clearDataLink'); 
 	clearDataLink.addEventListener("click", eraseLs); 
 
 
-	var storeButton = $('storeButton');
+	var storeButton = ge('storeButton');
 	storeButton.addEventListener("click", saveData);
 		console.log("storeData fired");
 	
